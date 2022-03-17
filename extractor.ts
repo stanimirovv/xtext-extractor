6e1688939576e8ca213e5a729d5c224e79451ab9
Zlatin Stanimirov <zlatin.stanimirov@adeccogroup.com>
2022-03-17 21:37:30 +0200

    feat: make extractor strategies extendable

--- extractor.ts
+++ extractor.ts
@@ -1,9 +1,40 @@
+import path from "path";
 import { AbstractStrategy } from "./strategy/abstractStrategy";
+import { PdfStrategy } from "./strategy/pdfStrategy";
+
+interface IStrategy {
+  execute(): string;
+}
 
 export class TextExtractor {
-  constructor(strategy: AbstractStrategy) {}
+  private static _supportedFileExtensions: Record<
+    string,
+    typeof AbstractStrategy
+  > = { ".pdf": PdfStrategy };
+
+  constructor(private readonly strategy: AbstractStrategy) {}
+
+  public static get supportedFileExtensions() {
+    return TextExtractor._supportedFileExtensions;
+  }
+
+  public static addExtractor(
+    extension: string,
+    strategy: typeof AbstractStrategy
+  ) {
+    TextExtractor._supportedFileExtensions[extension] = strategy;
+  }
+
+  public extract(): string {
+    return this.strategy.execute();
+  }
 }
 
 export function extractorFactory(filePath: string): TextExtractor {
-  return new TextExtractor(new AbstractStrategy(filePath));
+  const extension = path.extname(filePath);
+  const strategyClass = TextExtractor.supportedFileExtensions[extension];
+  if (!strategyClass) {
+    throw `Unsupported file type ${extension}`;
+  }
+  return new TextExtractor(new (strategyClass as any)(filePath));
 }
--- tests/extractor.spec.ts
+++ tests/extractor.spec.ts
@@ -1,8 +1,38 @@
 import { extractorFactory, TextExtractor } from "../extractor";
+import { AbstractStrategy } from "../strategy/abstractStrategy";
 
 describe("Extractor", () => {
   it("factory returns an extractor", () => {
-    const extractor = extractorFactory("myfile");
+    const extractor = extractorFactory("myfile.pdf");
     expect(extractor instanceof TextExtractor).toBeTruthy();
+  });
+
+  it("extractor can extract text", () => {
+    const extractor = extractorFactory("myfile.pdf");
+    expect(extractor.extract()).toEqual("hello");
+  });
+
+  it("extractor can extract text", () => {
+    expect.assertions(1);
+    try {
+      extractorFactory("myfile");
+    } catch (err) {
+      expect(err).toEqual("Unsupported file type");
+    }
+  });
+
+  it("add custom extractor, use it", () => {
+    class TestStrategy extends AbstractStrategy {
+      public execute(): string {
+        return "test";
+      }
+    }
+    TextExtractor.addExtractor(".ext", TestStrategy);
+    expect(Object.keys(TextExtractor.supportedFileExtensions).length).toEqual(
+      2
+    );
+    console.log(TextExtractor.supportedFileExtensions);
+    const extractor = extractorFactory("myfile.ext");
+    expect(extractor.extract()).toEqual("test");
   });
 });
